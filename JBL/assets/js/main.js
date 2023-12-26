280522da51255d1960df954b022f29035750db4d
myxtaridse <134769935+myxtaridse@users.noreply.github.com>
2023-12-26 14:41:04 +0300

    Add files via upload

---
+++ JBL/assets/js/main.js
@@ -0,0 +1,100 @@
+/*============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*============== REMOVE MENU MOBILE (нажатие на пункт из меню -> выходит из меню)===============*/
+const navLink = document.querySelectorAll('.nav-link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*============== ADD BLUR HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+/*============== SWIPER FAVORITES ===============*/
+let swiperFavorite = new Swiper('.favorite-swiper', {
+    loop: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+    grabCursor: true,
+
+    breakpoints:{
+        768:{
+            slidesPerView: 3,
+        }
+    }
+})
+
+/*============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                       : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+sr.reveal(`.home-social, .sponsor-container`)
+sr.reveal(`.home-j`, {origin: 'top', opacity: 1})
+sr.reveal(`.home-l`, {origin: 'bottom', opacity: 1})
+sr.reveal(`.home-tooltip`, {origin: 'right'})
+sr.reveal(`.home-button`, {origin: 'bottom'})
+sr.reveal(`.about-data`, {origin: 'left'})
+sr.reveal(`.about-img`, {origin: 'right'})
+sr.reveal(`.favorite-container`, {origin: 'bottom'})
+sr.reveal(`.model-tooltip`, {origin: 'right'})
+sr.reveal(`.model-button, .footer`, {origin: 'bottom'})
+
+
+
+
+
+
+
